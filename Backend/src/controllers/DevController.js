03adaa57facba806dd7f1f195ac565a14e1e468d
MarcosVRSDev <48337521+MarcosVRSDev@users.noreply.github.com>
2020-01-14 23:36:38 -0300

    Finalização Aula 2
    Correções de erros inicias e finalização da aula.

--- Backend/src/controllers/DevController.js
+++ Backend/src/controllers/DevController.js
@@ -1,23 +1,35 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+//Controller contém index, show, update, destroy
 
 module.exports = {
+    async index(request, response){
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
+
     async store(request, response) {
-    
         const{ github_username, techs, latitude, longitude } = request.body;
-    
+
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+            
         const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
     
         const { name = login, avatar_url, bio} = apiResponse.data;
     
-        const techsArray = techs.split(',').map(tech => tech.trim());
+        const techsArray = parseStringAsArray(techs);
     
         const location = {
             type: 'Point',
-            coordinates: [longitude, latitude],
+            coordinates: [longitude , latitude],
         };
     
-        const dev = await Dev.create({
+        dev = await Dev.create({
             github_username,
             name,
             avatar_url,
@@ -26,7 +38,17 @@ module.exports = {
             location,
         })
     
+
+        }
         return response.json(dev);
-    }
+    },
+
+    async update(request, response){
+
+    },
+
+    async destroy(request, response){
+        
+    },
 };
 
