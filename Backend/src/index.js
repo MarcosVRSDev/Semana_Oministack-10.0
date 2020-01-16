5a3ed73f4b7825164092db09f9c502e4f523f2dd
MarcosVRSDev <48337521+MarcosVRSDev@users.noreply.github.com>
2020-01-15 23:38:09 -0300

    Update do Front Web
    Desenvolvimento do fron-end Web.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,8 +9,10 @@ mongoose.connect('mongodb+srv://marcosrvrsdev:omnistack@cluster0-rcy9u.mongodb.n
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
 mongoose.set('useCreateIndex', true);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+// Componente: É uma fução que retorna um conteudo (html, css, js) - Ideal é um componente por arquivo // Bloco islado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+//Estado: Informações mantidas pelo componente (Lembrar do conceito de Imutabilidade)
+//Propriedade: Propriedades são os atributos do HTML// Informações que um componente PAI passa para o componente FILHO
+//<></> Fragment -> Serve para não acabar com estlização da página
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+
+  useEffect(() =>{
+    async function loadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data){
+
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+      <strong>Cadastrar</strong>
+      <DevForm onSubmit={handleAddDev}/>
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+        </ul>
+      </main>
+
     </div>
   );
 }
